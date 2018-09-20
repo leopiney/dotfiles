5c4b61b7d638c1eaa4c8b7a98a4bcc2214f78ed7
Leonardo Piñeyro <leopiney@gmail.com>
2018-09-20 18:30:32 -0300

    Minor changes in messages

--- git-utils.py
+++ git-utils.py
@@ -8,49 +8,49 @@ class GitUtils(object):
 
     def commit_to(self, to_branch, message, push=False, new_branch=False, rebase=False):
         current_branch = git('rev-parse', '--abbrev-ref', 'HEAD').strip()
-        print(u'📐 Committing changes in {} to {}'.format(current_branch, to_branch))
+        print(u'📐\tCommitting changes in {} to {}'.format(current_branch, to_branch))
 
         git('commit', m=message)
         commit_hash = git('rev-parse', 'HEAD').strip()
-        print(u'📝 Successfully created commit with hash "{}"'.format(commit_hash))
+        print(u'📝\tSuccessfully created commit with hash "{}"'.format(commit_hash))
 
         modified_files = git('status', '-s', '-uno').strip()
 
         if modified_files:
             git('stash')
-            print(u'👈 Stashed uncommited changes')
+            print(u'👈\tStashed uncommited changes')
 
         if new_branch:
             git('checkout', b=to_branch)
         else:
             git('checkout', to_branch)
-        print(u'🏃 Moved to branch "{}"'.format(to_branch))
+        print(u'🏃\tMoved to branch "{}"'.format(to_branch))
 
         git('cherry-pick', commit_hash)
-        print(u'🍒 Cherry-picked commit with hash "{}"'.format(commit_hash))
+        print(u'🍒\tCherry-picked commit with hash "{}"'.format(commit_hash))
 
         if push:
             git('push')
-            print(u'🙌 Pushed "{}"'.format(to_branch))
+            print(u'🙌\tPushed "{}"'.format(to_branch))
 
         git('checkout', current_branch)
-        print(u'🏃 Moved back to "{}"'.format(current_branch))
+        print(u'🏃\tMoved back to "{}"'.format(current_branch))
 
         if modified_files:
             git('stash', 'pop')
-            print(u'👉 Put back unstashed changes')
+            print(u'👉\tPut back unstashed changes')
 
         git('reset', '--hard', 'HEAD~1')
-        print(u'🗑  Removed new commit from "{}"'.format(current_branch))
+        print(u'🗑\tRemoved new commit from "{}"'.format(current_branch))
 
         if (rebase):
             git('rebase', to_branch)
-            print(u'🚜 Rebased commits from "{}" to the current branch "{}"'.format(
+            print(u'🚜\tRebased commits from "{}" to the current branch "{}"'.format(
                 to_branch,
                 current_branch,
             ))
 
-        print(u'🦄 Finished without errors')
+        print(u'🦄\tFinished without errors')
 
 
 if __name__ == '__main__':
